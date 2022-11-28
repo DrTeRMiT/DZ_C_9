fa152be8e4cddd23aade82680aec94a857b689f3
TeRMiT <termitspy@gmail.com>
2022-11-28 20:53:41 +0300

    решено дз

---
+++ task066/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers(int number1, int number2)
+{
+    if (number1 == number2)
+    {
+        return number1;
+    }
+    if (number1 > number2)
+    {
+        return number1 + SumNumbers(number1 - 1, number2);
+    }
+    else
+    {
+        return number1 + SumNumbers(number1 + 1, number2);
+    }
+}
+
+Console.Write("Введите число: m= ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: n= ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumbers(m, n);
+
+Console.WriteLine(sumNumbers);
---
+++ task068/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int AkkermanFunction(int number1, int number2)
+{
+    if (number1 == 0)
+    {
+        return number2 + 1;
+    }
+    if (number1 != 0 && number2 == 0)
+    {
+        return AkkermanFunction(number1 - 1, 1);
+    }
+    if (number1 > 0 && number2 > 0)
+    {
+        return AkkermanFunction(number1 - 1, AkkermanFunction(number1, number2 - 1));
+    }
+
+    return AkkermanFunction(number1, number2);
+}
+
+Console.Write("Введите число: m= ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: n= ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int akkermanFunction = AkkermanFunction(m, n);
+
+Console.WriteLine(akkermanFunction);
